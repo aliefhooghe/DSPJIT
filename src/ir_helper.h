ed3c17c87b7789b2809a3d840ba47d51f7e64202
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2019-11-06 21:22:49 +0100

    Enhance ir_helper

--- src/ir_helper.h
+++ src/ir_helper.h
@@ -11,33 +11,47 @@
 
 namespace ir_helper {
 
-    template <typename T>
-    llvm::Type *get_type(llvm::LLVMContext& context)
-    {
-        if constexpr (std::is_same_v<T, float>)
-            return llvm::Type::getFloatTy(context);
-        else if constexpr (std::is_same_v<T, double>)
-            return llvm::Type::getDoubleTy(context);
-        else 
-            static_assert(sizeof(T) < 8);
+    namespace runtime {
+
+        template <typename T>
+        llvm::Type *get_type(llvm::LLVMContext& context)
+        {
+            if constexpr (std::is_same_v<T, float>)             return llvm::Type::getFloatTy(context);
+            else if constexpr (std::is_same_v<T, double>)       return llvm::Type::getDoubleTy(context);
+            else if constexpr (std::is_same_v<T, uint32_t>)     return llvm::Type::getInt32Ty(context);
+        }
+
+        template <typename T>
+        llvm::Value *get_pointer(llvm::IRBuilder<>& builder, T *ptr)
+        {
+            constexpr auto ptr_size = sizeof(void*);
+            constexpr auto ptr_bit_count = ptr_size * 8;
+
+            auto& context = builder.getContext();
+            auto ptr_as_integer =
+                llvm::ConstantInt::get(context,
+                    llvm::APInt(ptr_bit_count, reinterpret_cast<intptr_t>(ptr)));
+
+            return builder.CreateIntToPtr(
+                ptr_as_integer,
+                llvm::PointerType::getUnqual(get_type<T>(context)));
+        }
+
+        template <typename T>
+        llvm::Value *create_load(llvm::IRBuilder<>& builder, T *ptr)
+        {
+            llvm::Value *ptr_value = get_pointer(ptr);
+            return builder.CreateLoad(ptr_value);
+        }
+
+        template <typename T>
+        void create_store(llvm::IRBuilder<>& builder, llvm::Value *val, T *ptr)
+        {
+            llvm::Value *ptr_value = get_pointer(ptr);
+            builder.CreateStore(val, ptr);
+        }
+
     }
-
-    template <typename T>
-    llvm::Value *get_pointer(llvm::IRBuilder<>& builder, T *ptr)
-    {
-        constexpr auto ptr_size = sizeof(ptr);
-        constexpr auto ptr_bit_count = ptr_size * 8;
-
-        auto& context = builder.getContext();
-        auto ptr_as_integer = 
-            llvm::ConstantInt::get(context, 
-                llvm::APInt(ptr_bit_count, reinterpret_cast<uint64_t>(ptr)));
-            
-        return builder.CreateIntToPtr(
-            ptr_as_integer, 
-            llvm::PointerType::getUnqual(get_type<T>(context)));
-    }
-
 }
 
 #endif

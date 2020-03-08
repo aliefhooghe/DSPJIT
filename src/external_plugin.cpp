c3ba0292c646c2e5475c0719019b8163516d6295
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2020-03-08 18:36:50 +0100

    Add external plugin loading. State are not handled for now.
    Rely on llvm bytecode or ir files.

---
+++ src/external_plugin.cpp
@@ -0,0 +1,246 @@
+
+#include <sstream>
+
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/Transforms/Utils/Cloning.h>
+
+#include "external_plugin.h"
+#include "ir_helper.h"
+
+#include <iostream>
+
+namespace DSPJIT {
+
+    //  Helper for name prefixing
+    static auto ptr_2_string(const void* ptr)
+    {
+        std::stringstream ss;
+        ss << ptr;
+        return  ss.str();
+    }
+
+    //
+    class external_plugin_node : public compile_node_class {
+
+    public:
+        explicit external_plugin_node(
+            unsigned int input_count,
+            unsigned int output_count,
+            const std::string& symbol,
+            std::size_t mutable_state_size);
+
+        std::vector<llvm::Value*> emit_outputs(
+                llvm::IRBuilder<>& builder,
+                const std::vector<llvm::Value*>& inputs,
+                llvm::Value *mutable_state_ptr) const override;
+    private:
+        llvm::FunctionType *create_func_type(llvm::IRBuilder<>& builder) const;
+        const std::string _symbol;
+    };
+
+    external_plugin_node::external_plugin_node(
+        unsigned int input_count,
+        unsigned int /*output_count*/,
+        const std::string& symbol,
+        std::size_t mutable_state_size)
+    :   compile_node_class{input_count, mutable_state_size},
+        _symbol{symbol}
+    {
+    }
+
+    std::vector<llvm::Value*> external_plugin_node::emit_outputs(
+        llvm::IRBuilder<>& builder,
+        const std::vector<llvm::Value*>& inputs,
+        llvm::Value *mutable_state_ptr) const
+    {
+        const auto input_count = get_input_count();
+        const auto output_count = 1u;
+
+        auto func_type = create_func_type(builder);
+        auto module = builder.GetInsertBlock()->getModule();
+        auto function = module->getOrInsertFunction(_symbol, func_type);
+
+        //  Allocate outputs
+        std::vector<llvm::Value*> outputs_ptr{output_count};
+
+        for (auto i = 0u; i < output_count; ++i)
+            outputs_ptr[i] = builder.CreateAlloca(builder.getFloatTy());
+
+        //  Call process func
+        std::vector<llvm::Value*> arg_values{};
+
+        arg_values.push_back(mutable_state_ptr != nullptr ?
+            builder.CreateIntToPtr(mutable_state_ptr, builder.getInt8PtrTy()) :
+            llvm::ConstantPointerNull::get(builder.getInt8PtrTy()));
+
+        //  Get arguments
+        for (auto i = 0u; i < input_count; ++i)
+            arg_values.push_back(inputs[i]);
+        for (auto i = 0u; i < output_count; ++i)
+            arg_values.push_back(outputs_ptr[i]);
+
+        //  Create call instruction
+        builder.CreateCall(function, arg_values);
+        // llvm::InlineFunctionInfo info;
+
+        // if (!llvm::InlineFunction(call_inst, info))
+        //     LOG_WARNING("[external_plugin_node] Unable to inline function");
+
+
+        //  Get and return Output values
+        std::vector<llvm::Value*> output_values{output_count};
+        for (auto i = 0u; i < output_count; ++i)
+            output_values[i] = builder.CreateLoad(outputs_ptr[i]);
+
+        return output_values;
+    }
+
+    llvm::FunctionType *external_plugin_node::create_func_type(llvm::IRBuilder<>& builder) const
+    {
+        const auto input_count = get_input_count();
+        const auto output_count = 1u;
+
+        //  Declare function
+        std::vector<llvm::Type*> params{builder.getInt8PtrTy()};
+
+        for (auto i = 0u; i < input_count; ++i)
+            params.push_back(builder.getFloatTy());
+        for (auto i = 0u; i < output_count; ++i)
+            params.push_back(llvm::PointerType::getUnqual(builder.getFloatTy()));
+
+        return llvm::FunctionType::get(builder.getVoidTy(), params, false);
+    }
+
+    //
+
+    external_plugin::external_plugin(
+        llvm::LLVMContext &llvm_context,
+        const std::vector<std::string>& code_object_paths,
+        const std::size_t mutable_state_size)
+    :   _mutable_state_size{mutable_state_size}
+    {
+        const auto symbol_prefix = "plugin__" + ptr_2_string(this) + "__";
+        bool process_func_found = false;
+
+        for (const auto& obj_path : code_object_paths) {
+            llvm::SMDiagnostic error;
+
+            auto module = llvm::parseIRFile(obj_path, error, llvm_context);
+            if (!module) {
+                LOG_ERROR("[external_plugin] Cannot load object %s", obj_path.c_str());
+                throw std::runtime_error("DSPJIT : Failed to load object");
+            }
+
+#ifndef NDEBUG
+        LOG_INFO("[external_plugin] Loaded module %s", obj_path.c_str());
+        ir_helper::print_module(*module);
+#endif
+            //  Add prefix for all global names in module
+            for(auto& function : *module) {
+                const auto new_name = symbol_prefix + function.getName();
+
+                if (function.getName().equals(process_func_symbol)) {
+                    if (process_func_found) {
+                        LOG_WARNING("[external_plugin] Warning : duplicate symbol %s", process_func_symbol);
+                        break;
+                    }
+
+                    process_func_found = true;
+                    _mangled_process_func_symbol = new_name.str();
+
+                    //  Check that function match an interface
+                    const auto process_func_type = function.getFunctionType();
+
+                    if (!_read_process_func_type(process_func_type)) {
+                        LOG_ERROR("[external_plugin] process function arguments does not match the required interface");
+                        throw std::runtime_error("");
+                    }
+
+                    LOG_DEBUG("[external_plugin] Found process symbol : %u input(s), %u output(s)", _input_count, _output_count);
+                }
+
+                function.setName(new_name);
+            }
+
+#ifndef NDEBUG
+        LOG_INFO("[external_plugin] Preprocessed module");
+        ir_helper::print_module(*module);
+#endif
+
+            _modules.emplace_back(std::move(module));
+        }
+
+        if (!process_func_found) {
+            LOG_ERROR("[external_plugin] Symbol '%s' not found in plugin", process_func_symbol);
+            throw std::runtime_error("DSPJIT : external_plugin : symbol not found");
+        }
+    }
+
+    void external_plugin::prepare_context(graph_execution_context& context)
+    {
+        //  Add every needed module to the executinon context
+        for (const auto& module : _modules)
+            context.add_module(llvm::CloneModule(*module));
+    }
+
+    std::unique_ptr<compile_node_class> external_plugin::create_node() const
+    {
+        return std::make_unique<external_plugin_node>(
+            _input_count,
+            _output_count,
+            _mangled_process_func_symbol,
+            _mutable_state_size);
+    }
+
+    bool external_plugin::_read_process_func_type(const llvm::FunctionType *func_type)
+    {
+        const auto param_count = func_type->getFunctionNumParams();
+
+        if (param_count <= 1)
+            return false;
+
+        //  Todo check first arg type
+
+        /*  */
+
+        //  void *, float ..., float *...
+        _input_count = 0u;
+        _output_count = 0u;
+        auto i = 1;
+
+        auto is_float_ptr = [] (const llvm::Type *t) {
+            const auto ptr_type = llvm::dyn_cast<llvm::PointerType>(t);
+            return ptr_type && ptr_type->getElementType()->isFloatTy();
+        };
+
+        //  Read inputs types : float...
+        for (; i < param_count; ++i) {
+            const auto param_type = func_type->getFunctionParamType(i);
+
+            if (param_type->isFloatTy()) {
+                _input_count++;
+            }
+            else if (is_float_ptr(param_type)) {
+                break;
+            }
+            else {
+                return false;
+            }
+        }
+
+        //  Read outputs types : float*
+        for (; i < param_count; ++i) {
+            const auto param_type = func_type->getFunctionParamType(i);
+
+            if(!is_float_ptr(param_type)) {
+                return false;
+            }
+
+            _output_count++;
+        }
+
+        return true;
+    }
+
+}
---
+++ src/external_plugin.h
@@ -0,0 +1,53 @@
+#ifndef EXTERNAL_PLUGIN_H_
+#define EXTERNAL_PLUGIN_H_
+
+#include <string>
+#include <vector>
+
+#include "compile_node_class.h"
+
+namespace DSPJIT {
+
+    /**
+     *  \class
+     *  \brief
+     *
+     *  node_process(void *state, float ...inputs, float* ...outputs)
+     *
+     **/
+    class external_plugin {
+        static constexpr auto process_func_symbol = "node_process";
+    public:
+        explicit external_plugin(
+            llvm::LLVMContext &llvm_context,
+            const std::vector<std::string>& code_object_paths,
+            const std::size_t mutable_state_size = 0u);
+
+        external_plugin(const external_plugin&) = delete;
+        external_plugin(external_plugin &&) = default;
+
+        /**
+         *
+         **/
+        void prepare_context(graph_execution_context& context);
+
+        /**
+         *
+         **/
+        std::unique_ptr<compile_node_class> create_node() const;
+
+    private:
+        bool _read_process_func_type(const llvm::FunctionType*);
+
+        using module_vector = std::vector<std::unique_ptr<llvm::Module>>;
+
+        const std::size_t _mutable_state_size;
+        unsigned int _input_count;
+        unsigned int _output_count;
+        std::string _mangled_process_func_symbol;
+        module_vector _modules{};
+    };
+
+}
+
+#endif

76c32b3eb79751ee317cc1db014ace3646add7f0
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2020-03-14 23:44:42 +0100

    Node disconect theirs output's users at desctruction
    and add node tests

--- src/node.h
+++ src/node.h
@@ -2,35 +2,83 @@
 #define JITTEST_NODE_H
 
 #include <vector>
+#include <unordered_set>
+#include <functional>
 
 namespace DSPJIT {
 
     template <typename Tderived>
     class node {
 
+        class input {
+            public:
+                input() = default;
+
+                input(const input&i)    {   plug(i._source);    }
+                input(input&& i)        {   plug(i._source);    }
+
+                ~input()
+                {
+                    unplug();
+                }
+
+                void plug(Tderived* n, unsigned int output_id)
+                {
+                    unplug();
+                    _source = n;
+                    _source->_users.insert(this);
+                    _output_id = output_id;
+                }
+
+                void unplug()
+                {
+                    if (_source == nullptr)
+                        return;
+
+                    _source->_users.erase(this);
+                    _source = nullptr;
+                }
+
+                auto get_output_id() const { return _output_id;   };
+                auto get_source()    const { return _source;      };
+
+            private:
+                Tderived *_source{nullptr};
+                unsigned int _output_id{0u};
+        };
+
+
     public:
         node(const unsigned int input_count) :
-            _input(input_count, nullptr)
+            _input(input_count)
         {}
 
-        virtual ~node() {}
+        virtual ~node()
+        {
+            //  this avoid iterators invalidation hazard
+            for (auto it = _users.begin(); it != _users.end(); (*it++)->unplug());
+        }
 
         void connect(Tderived& target, const unsigned int target_input)
         {
-            target._input[target_input] =
-                    static_cast<Tderived*>(this);
+            if (target_input >= target.get_input_count())
+                throw std::runtime_error("Node :  connect : invalid input");
+            target._input[target_input].plug(static_cast<Tderived*>(this), 0u);
         }
 
         Tderived *get_input(const unsigned int input_id) const
         {
-            return _input[input_id];
+            return _input[input_id].get_source();
         }
 
         const unsigned int get_input_count() const { return _input.size(); }
 
     private:
-        std::vector<Tderived*> _input;
+        std::unordered_set<input*> _users{};
+        std::vector<input> _input;
     };
+
+
 
 }
 
--- src/test.cpp
+++ src/test.cpp
@@ -12,6 +12,48 @@
 using namespace llvm;
 using namespace DSPJIT;
 
+
+/**
+ *
+ *      Node
+ *
+ *
+ **/
+
+class test_node : public node<test_node> {
+    public:
+        test_node(const unsigned int input_count):
+            node<test_node>(input_count)
+        {}
+};
+
+TEST_CASE("Node initial state", "node_initial_state")
+{
+    test_node n{2u};
+
+    REQUIRE(n.get_input_count() == 2u);
+    REQUIRE(n.get_input(0u) == nullptr);
+    REQUIRE(n.get_input(1u) == nullptr);
+}
+
+TEST_CASE("Node Conection", "node_connection")
+{
+    test_node n1{0u}, n3{2u};
+
+    {
+        test_node n2{0u};
+        n1.connect(n3, 0u);
+        n2.connect(n3, 1u);
+
+        REQUIRE(n3.get_input(0u) == &n1);
+        REQUIRE(n3.get_input(1u) == &n2);
+    }
+
+    REQUIRE(n3.get_input(0u) == &n1);
+    REQUIRE(n3.get_input(1u) == nullptr);
+}
+
+
 /**
  *
  *      Compile Node Class

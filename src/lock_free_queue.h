13ecd48ebd791bb4a7f95a566eeb00aff6bc31a0
Arthur Liefhooghe <aliefhooghe@enseirb-matmeca.fr>
2020-04-06 15:42:58 +0200

    Refactor lock free queue

--- src/lock_free_queue.h
+++ src/lock_free_queue.h
@@ -1,7 +1,7 @@
 #ifndef LOCK_FREE_QUEUE_H_
 #define LOCK_FREE_QUEUE_H_
 
-#include <cstddef> //  size_t
+#include <cstddef> //size_t
 #include <stdexcept>
 #include <vector>
 #include <atomic>
@@ -9,19 +9,19 @@
 
 /*
  *
- *      Lock free ONE Consumer - ONE Provider Queue
- * 
- * 
- * 
- *      -> [back|||||||||||||||front] ->
- *       enqueue               dequeue
+ *  Lock free ONE Consumer - ONE Provider Queue
+ *
+ *
+ *
+ *   -> [back|||||||||||||||front] ->
+ *   enqueue                        dequeue
  */
 
 template <class T>
 class lock_free_queue
 {
-
-  public:
+    static_assert(std::atomic<std::size_t> ::is_always_lock_free);
+public:
     lock_free_queue(const size_t capacity);
     lock_free_queue(const lock_free_queue<T> &) = delete;
     lock_free_queue(const lock_free_queue<T> &&) = delete;
@@ -33,7 +33,7 @@ class lock_free_queue
     bool enqueue(T &&);
 
     bool dequeue(T&);
-  private:
+private:
     const std::size_t m_capacity;
     std::atomic<std::size_t> m_write_ptr;
     std::atomic<std::size_t> m_read_ptr;
@@ -43,13 +43,13 @@ class lock_free_queue
 
 template <class T>
 lock_free_queue<T>::lock_free_queue(const size_t capacity)
-:   m_capacity(capacity), 
-    m_write_ptr(1u), 
+:   m_capacity(capacity),
+    m_write_ptr(1u),
     m_read_ptr(0u),
     m_data(capacity)
 {
-  if (capacity < 3)
-    throw std::domain_error("Invalid Queue capacity (size < 3)");
+    if (capacity < 3)
+        throw std::domain_error("Invalid Queue capacity (size < 3)");
 }
 
 template <class T>
@@ -58,58 +58,58 @@ lock_free_queue<T>::~lock_free_queue()
 }
 
 template <class T>
-inline size_t lock_free_queue<T>::capacity() const
+size_t lock_free_queue<T>::capacity() const
 {
-  return m_capacity;
+    return m_capacity;
 }
 
 template <class T>
-inline bool lock_free_queue<T>::enqueue(const T& x)
+bool lock_free_queue<T>::enqueue(const T& x)
 {
-  const size_t read_ptr = m_read_ptr.load();
-  const size_t write_ptr = m_write_ptr.load();
+    const size_t read_ptr = m_read_ptr.load();
+    const size_t write_ptr = m_write_ptr.load();
 
-  if (read_ptr == write_ptr)
-    return false;
+    if (read_ptr == write_ptr)
+        return false;
 
-  m_data[write_ptr] = x;
-  const size_t next_write_ptr = (write_ptr + 1) % m_capacity;
-  m_write_ptr.store(next_write_ptr);
+    m_data[write_ptr] = x;
+    const size_t next_write_ptr = (write_ptr + 1) % m_capacity;
+    m_write_ptr.store(next_write_ptr);
 
-  return true;
+    return true;
 }
 
 template <class T>
-inline bool lock_free_queue<T>::enqueue(T && x)
+bool lock_free_queue<T>::enqueue(T && x)
 {
-  const size_t read_ptr = m_read_ptr.load();
-  const size_t write_ptr = m_write_ptr.load();
+    const size_t read_ptr = m_read_ptr.load();
+    const size_t write_ptr = m_write_ptr.load();
 
-  if (read_ptr == write_ptr)  //  queue is full
-    return false;
+    if (read_ptr == write_ptr)    //    queue is full
+        return false;
 
-  m_data[write_ptr] = std::move(x);
-  const size_t next_write_ptr = (write_ptr + 1) % m_capacity;
-  m_write_ptr.store(next_write_ptr);
+    m_data[write_ptr] = std::move(x);
+    const size_t next_write_ptr = (write_ptr + 1) % m_capacity;
+    m_write_ptr.store(next_write_ptr);
 
-  return true;
+    return true;
 }
 
 template <class T>
-inline bool lock_free_queue<T>::dequeue(T & x)
+bool lock_free_queue<T>::dequeue(T & x)
 {
-  const size_t read_ptr = m_read_ptr.load();
-  const size_t write_ptr = m_write_ptr.load();
-  const size_t next_read_ptr = (read_ptr + 1) % m_capacity;
+    const size_t read_ptr = m_read_ptr.load();
+    const size_t write_ptr = m_write_ptr.load();
+    const size_t next_read_ptr = (read_ptr + 1) % m_capacity;
 
-  if (next_read_ptr == write_ptr)   //  queue is empty
-    return false;
+    if (next_read_ptr == write_ptr)     //    queue is empty
+        return false;
 
-  
-  x = std::move(m_data[next_read_ptr]);
-  m_read_ptr.store(next_read_ptr);
 
-  return true;
+    x = std::move(m_data[next_read_ptr]);
+    m_read_ptr.store(next_read_ptr);
+
+    return true;
 }
 
 
